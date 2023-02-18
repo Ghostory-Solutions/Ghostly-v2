ed8f3da19cf41184973b81469f991890e440df14
Cat <89495053+aRealCatTBH@users.noreply.github.com>
2023-02-17 20:13:15 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,14 +1,11 @@
 (function() {
-  const endpoint = document.currentScript.getAttribute("data-api");
-  const referrer = document.referrer || "Direct";
-  const device = width > 1440 ? "Desktop" : width > 992 ? "Laptop" : width > 576 ? "Tablet" : "Mobile";
-  fetch(endpoint, {
+  fetch(document.currentScript.getAttribute("data-api"), {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       page: window.location.pathname,
-      referrer,
-      device,
+      referrer: document.referrer || "Direct",
+      device: width > 1440 ? "Desktop" : width > 992 ? "Laptop" : width > 576 ? "Tablet" : "Mobile",
     }),
   });
 })();
