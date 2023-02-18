322645d94dd8a4bcf424149631d47c58377f08c2
Cat <89495053+aRealCatTBH@users.noreply.github.com>
2023-02-17 20:28:34 -0500

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
-(function() {
-  fetch(document.currentScript.getAttribute("data-api"), {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      page: window.location.pathname,
-      referrer: document.referrer || "Direct",
-      device: width > 1440 ? "Desktop" : width > 992 ? "Laptop" : width > 576 ? "Tablet" : "Mobile",
-    }),
-  });
+(function () {
+    const w = Math.max(document.body.scrollWidth, document.documentElement.scrollWidth, document.body.offsetWidth, document.documentElement.offsetWidth, document.documentElement.clientWidth, window.innerWidth);
+    fetch(document.currentScript.getAttribute("data-api"), {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            page: window.location.pathname,
+            referrer: document.referrer || "Direct",
+            device: w > 1440 ? "Desktop" : w > 992 ? "Laptop" : w > 576 ? "Tablet" : "Mobile",
+        }),
+    });
 })();
